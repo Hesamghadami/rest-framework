5f7f3909f71baca403b21fdd3cf2ceb07591a66f
Hesam <hesam.ghadami2020@gmail.com>
2023-10-17 20:20:36 +0330

    first-step

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+app_name = 'accounts'
+
+
+urlpatterns = [
+    path('login/', Login, name='login'),
+    path('logout/', Logout, name='logout'),
+    path('signup/', signup, name='signup'),
+]
